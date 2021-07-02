1363eda232979f40180894c6beadd04d8b857f92
Alice-Retz <alicegretz@gmail.com>
2021-07-02 15:33:33 -0500

    First revisions: Rehaul of results function. Additional html work.

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-// import functions and grab DOM elements
-import { randomNumber, getRandomThrow, doesUserWin } from './get-random-throw.js';
-// initialize state
-const submit = document.getElementById('submit');
-// set event listeners 
-// get user input
-// use user input to update state 
-// update DOM to reflect the new state
-submit.addEventListener('click', ()=>{
-    let num = randomNumber();
-    let computerThrow = getRandomThrow(num);
-    const selected = document.querySelector('input[type=radio]:checked');
-    const userThrow = selected.value; 
-    doesUserWin(userThrow, computerThrow);
-});
+//import documents
+import { randomNumber, getRandomThrow } from './get-random-throw.js';
+import { doesUserWin } from './does-user-win.js';
+//DOM elements
+const submitBtn = document.getElementById('submit');
+const computerThrow = document.getElementById('computer-choice');
+const gameResults = document.getElementById('outcome');
+const winSpan = document.getElementById('total-wins');
+const lossSpan = document.getElementById('total-loss');
+const drawSpan = document.getElementById('total-draws');
+const resetSpan = document.getElementById('reset-total');
+const resetBtn = document.getElementById('reset-button');
+
+let win = 0;
+let loss = 0;
+let draw = 0;
+let reset = 0;
+
+
+submitBtn.addEventListener('click', () => {
+    const userInput = document.querySelector('input[type=radio]:checked');
+    const userThrow = userInput.value;
+    let computerThrow = getRandomThrow();
+    const results = doesUserWin();
+})
