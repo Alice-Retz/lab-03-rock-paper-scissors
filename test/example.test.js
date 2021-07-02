b97f17958b6820c3cf6784cc41b38b26de03f2ab
Alice-Retz <alicegretz@gmail.com>
2021-07-02 17:14:30 -0500

    All TDD finished.

--- test/example.test.js
+++ test/example.test.js
@@ -1,6 +1,7 @@
 const test = QUnit.test;
 
 import { getRandomThrow } from '../get-random-throw.js';
+import { doesUserWin } from '../does-user-win.js';
 
 
 test('does 1 = rock', (expect) => {
@@ -25,11 +26,70 @@ test('does 3 = scissors', (expect) => {
 
 test('does rock beat scissors', (expect) => {
 
-const user = 'rock'; 
-const computer = 'scissors';
-const expected = 'win';
-const actual = 'doesUserWin(user, computer)'; 
-expect.equal(actual, expected); 
+    const user = 'rock'; 
+    const computer = 'scissors';
+    const expected = 'win';
+    const actual = doesUserWin(user, computer); 
+    expect.equal(actual, expected); 
 
-})
+});
 
+test('does rock beat paper', (expect) => {
+
+    const user = 'rock'; 
+    const computer = 'paper';
+    const expected = 'loss';
+    const actual = doesUserWin(user, computer); 
+    expect.equal(actual, expected); 
+
+});
+
+test('does paper beat rock', (expect) => {
+
+    const user = 'paper'; 
+    const computer = 'rock';
+    const expected = 'win';
+    const actual = doesUserWin(user, computer); 
+    expect.equal(actual, expected); 
+
+});
+
+test('does paper beat scissors', (expect) => {
+
+    const user = 'paper'; 
+    const computer = 'scissors';
+    const expected = 'loss';
+    const actual = doesUserWin(user, computer); 
+    expect.equal(actual, expected); 
+
+});
+
+test('does scissors beat paper', (expect) => {
+
+    const user = 'scissors'; 
+    const computer = 'paper';
+    const expected = 'win';
+    const actual = doesUserWin(user, computer); 
+    expect.equal(actual, expected); 
+
+});
+
+test('does scissors beat rock', (expect) => {
+
+    const user = 'scissors'; 
+    const computer = 'rock';
+    const expected = 'loss';
+    const actual = doesUserWin(user, computer); 
+    expect.equal(actual, expected); 
+
+});
+
+test('does scissors beat scissors', (expect) => {
+
+    const user = 'scissors'; 
+    const computer = 'scissors';
+    const expected = 'draw';
+    const actual = doesUserWin(user, computer); 
+    expect.equal(actual, expected); 
+
+});
