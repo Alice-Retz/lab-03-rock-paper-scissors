dfcc4ea52ab2f7a7a144cee3b6b586ff5636232c
Alice-Retz <alicegretz@gmail.com>
2021-07-02 20:10:04 -0500

    Troubleshooting app.js, now fixed.

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 //import documents
-import { getRandomThrow } from './get-random-throw.js';
+import { getRandomThrow, randomNumber } from './get-random-throw.js';
 import { doesUserWin } from './does-user-win.js';
 //DOM elements
 const submitBtn = document.getElementById('submit');
-const computerThrow = document.getElementById('computer-choice');
+const computerChoice = document.getElementById('computer-choice');
 const gameResults = document.getElementById('outcome');
 const winSpan = document.getElementById('total-wins');
 const lossSpan = document.getElementById('total-loss');
 const drawSpan = document.getElementById('total-draws');
-const resetSpan = document.getElementById('reset-total');
+const resetSpan = document.getElementById('total-resets');
 const resetBtn = document.getElementById('reset-button');
 
 let win = 0;
@@ -20,8 +20,10 @@ let reset = 0;
 submitBtn.addEventListener('click', () => {
     const userInput = document.querySelector('input[type=radio]:checked');
     const userThrow = userInput.value;
-    let computerThrow = getRandomThrow();
+    const randomNum = randomNumber();
+    let computerThrow = getRandomThrow(randomNum);
     const results = doesUserWin(userThrow, computerThrow);
+    computerChoice.textContent = `${computerThrow}`;
     if (results === 'win') {
         win++;
         gameResults.textContent = `You won!`;
@@ -35,4 +37,17 @@ submitBtn.addEventListener('click', () => {
     } else {
         gameResults.textContent = `Whoops, something went wrong! Let's try again.`;
     }
-})
+    winSpan.textContent = `${win}`;
+    lossSpan.textContent = `${loss}`;
+    drawSpan.textContent = `${draw}`;
+});
+resetBtn.addEventListener('click', () => {
+    reset++;
+    win = 0;
+    winSpan.textContent = `${win}`;
+    loss = 0;
+    lossSpan.textContent = `${loss}`;
+    draw = 0;
+    drawSpan.textContent = `${draw}`;
+    resetSpan.textContent = `${reset}`;
+});
